55393ce083477c831a54c6fa2fcb2913da3be8aa
mariahuertas <mariahuertas296@gmail.com>
2020-07-30 20:29:56 +0200

    Add DAO in Views folder

--- draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/SaveView.java
+++ draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/SaveView.java
@@ -1,35 +1,31 @@
 package es.urjccode.mastercloudapps.adcs.draughts.views;
 
 import es.urjccode.mastercloudapps.adcs.draughts.controllers.SaveController;
+import es.urjccode.mastercloudapps.adcs.draughts.utils.IO;
 import es.urjccode.mastercloudapps.adcs.draughts.views.Message;
 import es.urjccode.mastercloudapps.adcs.draughts.utils.YesNoDialog;
+
 class SaveView {
+
     void interact(SaveController saveController) {
+        boolean save = new YesNoDialog().read(Message.SAVE);
+        String name = null;
+        if (save) {
+            if (saveController.hasName()) {
+                saveController.save();
+            } else {
+                boolean exists = false;
+                do {
+                    name = IO.readString(Message.NAME);
+                    exists = saveController.exists(name);
+                    if (exists) {
+                        IO.writeln(Message.NAME_EXISTS);
+                    }
+                } while (exists);
+                saveController.save(name);
+            }
+        }
+        saveController.next();
     }
 }
-/*
-class SaveView extends WithConsoleView {
 
-	void interact(SaveController saveController) {
-		boolean save = new YesNoDialog().read(MessageView.SAVE.getMessage());
-		String name = null;
-		if (save) {
-			if (saveController.hasName()) {
-				saveController.save();
-			} else {
-				boolean exists = false;
-				do {
-					name = this.console.readString(MessageView.NAME.getMessage());
-					exists = saveController.exists(name);
-					if (exists) {
-						this.console.writeln("The proposed name already exists");
-					}
-				} while (exists);
-				saveController.save(name);
-			}
-		}
-		saveController.next();
-	}
-
-}
- */
