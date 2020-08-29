2afc3a14b2e3f69246abd570f30632a974345287
mariahuertas <mariahuertas296@gmail.com>
2020-08-29 12:12:51 +0200

    Delete dobles directory

--- draughts/src/main/java/es/urjccode/mastercloudapps/adcs/dobles/initMocks/DOC.java
+++
@@ -1,9 +0,0 @@
-package es.urjccode.mastercloudapps.adcs.dobles.initMocks;
-
-public class DOC {
-
-	public int exerciseDOC(boolean value) {
-        return 0;
-	}
-    
-}
--- draughts/src/main/java/es/urjccode/mastercloudapps/adcs/dobles/runWith/DOC.java
+++
@@ -1,9 +0,0 @@
-package es.urjccode.mastercloudapps.adcs.dobles.runWith;
-
-public class DOC {
-
-	public int exerciseDOC(boolean value) {
-        return 0;
-	}
-    
-}
